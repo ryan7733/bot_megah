4da07ab9a0b621b04274737114a0f79d4560df53
Ryan <76922513+ryan7733@users.noreply.github.com>
2021-01-08 19:13:14 -0300

    Add files via upload

---
+++ dark-main/src/darkmenu.js
@@ -0,0 +1,40 @@
+const megahmenu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *Comandos Da bot_megah:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}off
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *Bot_Megah*
+  DUVIDAS? 👇
+  Off
+╚════════════════════`
+}
+
+exports.bot_Megamenu = bot_megamenu
+
+
+
+
+
+
+
+
